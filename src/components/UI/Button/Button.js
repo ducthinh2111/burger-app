f22db5d7199f0bf908241344ddb4ad00c85477e8
Le Duc Thinh <thinhle2199@gmail.com>
2020-09-23 22:36:26 +0700

    Add validation to the order form and integrate with database

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Button.module.css";
 
 const button = (props) => (
-  <button className={[classes.Button, classes[props.btnType]].join(' ')} onClick={props.onClick}>
+  <button disabled={props.disabled} className={[classes.Button, classes[props.btnType]].join(' ')} onClick={props.onClick}>
     {props.children}
   </button>
 );
