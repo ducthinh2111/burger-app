271cafb17a041ec0c7613c44abf3a5ec764c52bc
Le Duc Thinh <thinhle2199@gmail.com>
2020-10-01 15:23:38 +0700

    Merge branch develop

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Auxiliary from "../Auxiliary/Auxiliary";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
